8c000a6bae83885981d78803d98db581032cb2bd
Nick Pannucci <pan1322@gmail.com>
2014-01-29 20:48:08 -0800

    spaces

--- Chapter_10/chapter10.6_exercises.rb
+++ Chapter_10/chapter10.6_exercises.rb
@@ -1,68 +1,68 @@
 # English number exercises
 
 def english_number(number)
-	if number < 0
-		return "Please enter a number that isn't negative."
-	end
-	if number == 0
-		return "zero"
-	end
+  if number < 0
+    return "Please enter a number that isn't negative."
+  end
+  if number == 0
+    return "zero"
+  end
 
-	num_string = ''
+  num_string = ''
 
-	ones_place = %w{one two three four five six seven eight nine}
-	tens_place = %w{ten twenty thirty forty fifty sixty seventy eighty ninety}
-	teenagers =  %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
+  ones_place = %w{one two three four five six seven eight nine}
+  tens_place = %w{ten twenty thirty forty fifty sixty seventy eighty ninety}
+  teenagers =  %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
 
-	left  = number
-	write = left/1000
-	left = left - write*1000
+  left  = number
+  write = left/1000
+  left = left - write*1000
 
-	if write > 0
-		thousands = english_number write
-		num_string = num_string + thousands + ' thousand'
-		if left > 0
-			num_string = num_string + ' '
-		end
-	end
+  if write > 0
+    thousands = english_number write
+    num_string = num_string + thousands + ' thousand'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
 
-	write = left/100
-	left  = left - write*100
+  write = left/100
+  left  = left - write*100
 
-	if write > 0
-		hundreds = english_number write
-		num_string = num_string + hundreds + ' hundred'
-		if left > 0
-			num_string = num_string + ' '
-		end
-	 end
+  if write > 0
+    hundreds = english_number write
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      num_string = num_string + ' '
+    end
+   end
 
-	 write = left/10
-	 left = left - write*10
+   write = left/10
+   left = left - write*10
 
-	 if write > 0
-	 	if ((write == 1) and (left > 0))
-	 		num_string = num_string + teenagers[left-1]
-	 		left = 0
-	 	else
-	 		num_string = num_string + tens_place[write-1]
-	 	end
+   if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
 
-	 	if left > 0
-	 		num_string = num_string + '-'
-	 	end
-	 end
+    if left > 0
+      num_string = num_string + '-'
+    end
+   end
 
-	 write = left
-	 left = 0
+   write = left
+   left = 0
 
-	 if write > 0
-	 	num_string = num_string + ones_place[write-1]
-	 end
+   if write > 0
+    num_string = num_string + ones_place[write-1]
+   end
 
-	num_string
+  num_string
 end
 
-puts english_number(165)
+puts english_number(190)
 puts english_number(15)
 
--- Chapter_10/chapter10.7_exercises.rb
+++ Chapter_10/chapter10.7_exercises.rb
@@ -1,85 +1,85 @@
 # 99 Bottles - Recursion
 
 def english_number(number)
-	if number < 0
-		return "Please enter a number that isn't negative."
-	end
-	if number == 0
-		return "zero"
-	end
+  if number < 0
+    return "Please enter a number that isn't negative."
+  end
+  if number == 0
+    return "zero"
+  end
 
-	num_string = ''
+  num_string = ''
 
-	ones_place = %w{one two three four five six seven eight nine}
-	tens_place = %w{ten twenty thirty forty fifty sixty seventy eighty ninety}
-	teenagers =  %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
+  ones_place = %w{one two three four five six seven eight nine}
+  tens_place = %w{ten twenty thirty forty fifty sixty seventy eighty ninety}
+  teenagers =  %w{eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen}
 
-	left  = number
-	write = left/1000
-	left = left - write*1000
+  left  = number
+  write = left/1000
+  left = left - write*1000
 
-	if write > 0
-		thousands = english_number write
-		num_string = num_string + thousands + ' thousand'
-		if left > 0
-			num_string = num_string + ' '
-		end
-	end
+  if write > 0
+    thousands = english_number write
+    num_string = num_string + thousands + ' thousand'
+    if left > 0
+      num_string = num_string + ' '
+    end
+  end
 
-	write = left/100
-	left  = left - write*100
+  write = left/100
+  left  = left - write*100
 
-	if write > 0
-		hundreds = english_number write
-		num_string = num_string + hundreds + ' hundred'
-		if left > 0
-			num_string = num_string + ' '
-		end
-	 end
+  if write > 0
+    hundreds = english_number write
+    num_string = num_string + hundreds + ' hundred'
+    if left > 0
+      num_string = num_string + ' '
+    end
+   end
 
-	 write = left/10
-	 left = left - write*10
+   write = left/10
+   left = left - write*10
 
-	 if write > 0
-	 	if ((write == 1) and (left > 0))
-	 		num_string = num_string + teenagers[left-1]
-	 		left = 0
-	 	else
-	 		num_string = num_string + tens_place[write-1]
-	 	end
+   if write > 0
+    if ((write == 1) and (left > 0))
+      num_string = num_string + teenagers[left-1]
+      left = 0
+    else
+      num_string = num_string + tens_place[write-1]
+    end
 
-	 	if left > 0
-	 		num_string = num_string + '-'
-	 	end
-	 end
+    if left > 0
+      num_string = num_string + '-'
+    end
+   end
 
-	 write = left
-	 left = 0
+   write = left
+   left = 0
 
-	 if write > 0
-	 	num_string = num_string + ones_place[write-1]
-	 end
+   if write > 0
+    num_string = num_string + ones_place[write-1]
+   end
 
-	num_string.capitalize
+  num_string.capitalize
 end
 
 def bottles_of_beer(number=10)
-	if number > 0
-		if number == 1
-			puts "#{english_number(number)} bottle of beer on the wall. #{english_number(number)} bottle of beer. \nTake one down and pass it around...no more bottles of beer on the wall."
-			number -= 1
-		end
-		if number == 2
-			puts "#{english_number(number)} bottles of beer on the wall. #{english_number(number)} bottles of beer. \nTake one down and pass it around...#{english_number(number - 1)} bottle of beer on the wall."
-			number -=1
-			bottles_of_beer(number)
-		end
-		if number > 1
-			puts "#{english_number(number)} bottles of beer on the wall. #{english_number(number)} bottles of beer. \nTake one down and pass it around.... #{english_number(number-1)} bottles of beer on the wall."
-			number -= 1
-			bottles_of_beer(number)
-		end
-	end
+  if number > 0
+    if number == 1
+      puts "#{english_number(number)} bottle of beer on the wall. #{english_number(number)} bottle of beer. \nTake one down and pass it around...no more bottles of beer on the wall."
+      number -= 1
+    end
+    if number == 2
+      puts "#{english_number(number)} bottles of beer on the wall. #{english_number(number)} bottles of beer. \nTake one down and pass it around...#{english_number(number - 1)} bottle of beer on the wall."
+      number -=1
+      bottles_of_beer(number)
+    end
+    if number > 1
+      puts "#{english_number(number)} bottles of beer on the wall. #{english_number(number)} bottles of beer. \nTake one down and pass it around.... #{english_number(number-1)} bottles of beer on the wall."
+      number -= 1
+      bottles_of_beer(number)
+    end
+  end
 end
 
 bottles_of_beer(20)
